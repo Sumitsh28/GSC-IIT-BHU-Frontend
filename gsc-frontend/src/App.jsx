171bbed8970e55e6ff0fad642c2e02d827635cec
Akash_Ktr <akash.katiyar.met21@itbhu.ac.in>
2023-12-17 19:59:55 +0530

    clean up/ minor changes

--- gsc-frontend/src/App.jsx
+++ gsc-frontend/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from '/assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import gscLogo from '/assets/gscLogo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='text-center w-48 m-auto'>
+        <img className='' src={gscLogo} alt="GnSC Logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='text-center w-54 m-auto'>
+        <h1 className='text-3xl font-bold'>Games & Sports Council</h1>
+        <h1 className='text-3xl font-bold'>IIT (BHU) Varanasi</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
