e20585e1eb77e47d67e8a4ec270c7083c99b453f
Rashmik <rashmik2705@gmail.com>
2024-01-31 01:47:00 +0530

    Add responsive images in stats

--- gsc-frontend/src/components/Stats.jsx
+++ gsc-frontend/src/components/Stats.jsx
@@ -3,9 +3,10 @@ import Heart from "/assets/Heart.svg";
 import Temple from "/assets/Temple.svg";
 import Individual from "/assets/Individual.svg";
 import Team from "/assets/Team.svg";
+import "../App.css";
 import { useInView } from "react-intersection-observer";
 const Stats = () => {
-  const a = 20; // Fetch from backend from hook 
+  const a = 20; // Fetch from backend from hook
   const [core, setCore] = useState(0);
   const [institute, setInstitute] = useState(0);
   const [clubs, setClubs] = useState(0);
@@ -13,20 +14,20 @@ const Stats = () => {
   const { ref, inView } = useInView({
     threshold: 0.3,
   });
-  useEffect(() => { 
+  useEffect(() => {
     let r;
-    console.log('Hello');
-    if(inView && core<a && institute<a && clubs<a && events<a){
-      r = setInterval(()=>{
-        setCore(c=>c+1);
-        setInstitute(i=>i+1);
-        setClubs(c=>c+1);
-        setEvents(e=>e+1);
-      },100)
+    // console.log('Hello');
+    if (inView && core < a && institute < a && clubs < a && events < a) {
+      r = setInterval(() => {
+        setCore((c) => c + 1);
+        setInstitute((i) => i + 1);
+        setClubs((c) => c + 1);
+        setEvents((e) => e + 1);
+      }, 100);
     }
-    return ()=>clearInterval(r);
-  }, [clubs,core,institute,events,inView]);
-  
+    return () => clearInterval(r);
+  }, [clubs, core, institute, events, inView]);
+
   return (
     //For large screens
     <div ref={ref} className="bg-statsbg ">
@@ -35,14 +36,14 @@ const Stats = () => {
           <div className="bg-[url('/assets/DiscusThrower.svg')] bg-no-repeat bg-right-bottom bg-18%">
             <div className="flex justify-evenly pt-32 pb-40">
               <div className="text-center font-inter flex flex-col">
-                <img style={{ height: "100px" }} src={Heart} alt="Heart" />
+                <img className="image-stats" src={Heart} alt="Heart" />
                 <div className="text-white font-bold text-3xl pt-6">{core}</div>
                 <div className="text-white font-semibold text-2xl pt-2">
                   Core Team Members
                 </div>
               </div>
               <div className="text-center flex flex-col">
-                <img style={{ height: "100px" }} src={Team} alt="Team" />
+                <img className="image-stats" src={Team} alt="Team" />
                 <div className="text-white font-bold text-3xl pt-6">
                   {institute}
                 </div>
@@ -51,7 +52,7 @@ const Stats = () => {
                 </div>
               </div>
               <div className="text-center flex flex-col">
-                <img style={{ height: "100px" }} src={Temple} alt="Temple" />
+                <img className="image-stats" src={Temple} alt="Temple" />
 
                 <div className="text-white font-bold text-3xl pt-6">
                   {clubs}
@@ -62,7 +63,7 @@ const Stats = () => {
               </div>
               <div className="text-center flex flex-col">
                 <img
-                  style={{ height: "100px" }}
+                  className="image-stats"
                   src={Individual}
                   alt="Individual"
                 />
