0f516a8dbaacab9a423cd3a1e4e5063b4704252f
Sumitsh28 <sumit.off28@gmail.com>
2024-02-06 19:52:09 +0530

    Video Player fix

--- gsc-frontend/src/components/Video.jsx
+++ gsc-frontend/src/components/Video.jsx
@@ -1,9 +1,9 @@
 const Video = () => {
   return (
     <>
-      <div className="flex justify-center items-center w-screen h-screen bg-black p-4">
+      <div className="flex justify-center items-center smd:w-screen smd:h-screen h-[22vh] bg-black p-4">
         <iframe
-          className="rounded-lg w-screen h-screen"
+          className="rounded-lg smd:w-screen smd:h-screen h-[22vh] w-[90vw]"
           src="https://www.youtube.com/embed/5rVw16UHLNw?autoplay=1&mute=1"
           title="Spardha&#39;18 | Aftermovie"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
