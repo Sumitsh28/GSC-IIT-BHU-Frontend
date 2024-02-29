779d647d7d8d1c0c780ea465ae6d2fe9a1410e81
vipul <vipulkumar2426@gmail.com>
2024-02-29 20:04:37 +0530

    sports pg

--- gsc-frontend/src/pages/Sports.jsx
+++ gsc-frontend/src/pages/Sports.jsx
@@ -1,8 +1,132 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
-
+import TeamCard from "../components/TeamCard";
+import cricket from "../assets/cricket.png"
 function Sports() {
   const [scrolling, setScrolling] = useState(false);
+  const data = [
+    {
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },
+  ];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -22,7 +146,41 @@ function Sports() {
   return (
     <>
       <Navbar scrolling={scrolling} />
-      <div>Teams & Clubs</div>
+      <div
+        className=""
+        style={{ background: "linear-gradient(to right, #ffe270, #ff914d)" }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "32%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            fontSize: "24vw",
+            color: "antiquewhite",
+            opacity:'0.7',
+            fontFamily:'lexend zetha',
+            fontWeight:"bold"
+          }}
+        >
+          Sports
+        </div>
+        <div className="flex items-center flex-wrap justify-center sm:gap-20 pt-72 pb-20 px-10">
+          {data.map((member) => (
+            <div>
+              <TeamCard
+                name={member.name}
+                image={member.imageUrl}
+                gmail={member.email}
+                instagram={member.instagram}
+                linkedin={member.linkedin}
+                facebook={member.facebook}
+                twitter={member.twitter}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
