92407f5f130ca1ce7664201cce4ceb89d0aed6fb
Sumitsh28 <sumit.off28@gmail.com>
2024-03-23 22:20:29 +0530

    Whitespace in individual Sports page

--- gsc-frontend/src/components/Image.jsx
+++ gsc-frontend/src/components/Image.jsx
@@ -3,25 +3,25 @@ import image1 from "../images/cricket.svg";
 const ImageStack = () => {
   return (
     <>
-      <div className="sm:flex hidden">
-        <div className=" flex flex-row gap-7">
-          <div className="flex flex-col items-center ">
-            <img src={image1} alt="Image 1" className="w-[570px] h-[316px]" />
-            <p className="text-center font-lexend text-md w-[510px] border-2 border-black">
+      <div className="sm:flex hidden items-center justify-center">
+        <div className=" flex flex-row gap-7 items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
+            <img src={image1} alt="Image 1" className="w-[450px] h-[280px]" />
+            <p className="text-center font-lexend text-md w-[450px] border-2 border-black">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut
             </p>
           </div>
           <div className="flex flex-col items-center">
-            <img src={image1} alt="Image 1" className="w-[570px] h-[316px] " />
-            <p className="text-center font-lexend text-md w-[510px] border-2 border-black">
+            <img src={image1} alt="Image 1" className="w-[450px] h-[280px] " />
+            <p className="text-center font-lexend text-md w-[450px] border-2 border-black">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut
             </p>
           </div>
           <div className="flex flex-col items-center">
-            <img src={image1} alt="Image 1" className="w-[570px] h-[316px] " />
-            <p className="text-center font-lexend text-md w-[510px] border-2 border-black">
+            <img src={image1} alt="Image 1" className="w-[450px] h-[280px] " />
+            <p className="text-center font-lexend text-md w-[450px] border-2 border-black">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut
             </p>
