12d5e44469a4485196a1e85c58d83b39f843c956
Sumitsh28 <sumit.off28@gmail.com>
2024-03-29 21:52:17 +0530

    Individual Sports Page Updated

---
+++ gsc-frontend/src/components/CricketHero.jsx
@@ -0,0 +1,90 @@
+import image1 from "../images/cricket.svg";
+import pattern1 from "../images/pattern1.svg";
+import pattern2 from "../images/pattern2.svg";
+
+function CricketHero() {
+  return (
+    <div>
+      {/* Desktop */}
+      <div
+        className="hidden h-[530px] lg:h-[704px] md:h-[560px] sm:h-[460px] sm:flex flex-row  justify-center sm:gap-[40px] relative z-[-2]"
+        style={{
+          backgroundImage: `url(${image1})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "100%",
+          height: "894px",
+        }}
+      >
+        <div className=" sm:flex flex-col lg:mt-[90px] md:mt-[120px] sm:mt-[100px] items-center gap-[15rem]">
+          <p className=" lg:text-[100px] md:text-[40.5px] sm:text-[25.5px] text-white font-bold font-lexend tracking-tight text-center mt-24">
+            Cricket
+          </p>
+          <div className="relative bg-[#6690B8] p-4 w-[1200px] h-[600px] font-lexend text-center mt-20 ">
+            <img
+              src={pattern1}
+              className="absolute top-5 left-5 z-10"
+              style={{ width: "181px", height: "181px" }}
+            />
+            <img
+              src={pattern2}
+              className="absolute bottom-5 right-5 z-10"
+              style={{ width: "181px", height: "181px" }}
+            />
+            <h2 className="text-black text-5xl mb-4 font-bold relative z-20 mt-5">
+              About
+            </h2>
+            <p className="text-black text-2xl tracking-wider font-thin relative z-20 mb-16 mt-5 w-[1000px] left-20">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile */}
+      <div
+        className="sm:hidden h-[530px] lg:h-[704px] md:h-[560px] sm:h-[460px] flex flex-row  justify-center sm:gap-[40px] relative z-[-2]"
+        style={{
+          backgroundImage: `url(${image1})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "100%",
+          height: "284px",
+        }}
+      >
+        <div className="justify-center relative h-[900px]">
+          <p className="w-[100%] text-[60px] text-white font-bold font-lexend tracking-tight text-center mt-16">
+            Cricket
+          </p>
+          <div className="relative bg-[#6690B8] p-4 w-[324px] h-[200px] font-lexend text-center mt-10 ">
+            <img
+              src={pattern1}
+              className="absolute top-2 left-2 z-10"
+              style={{ width: "80px", height: "80px" }}
+            />
+            <img
+              src={pattern2}
+              className="absolute bottom-2 right-2 z-10"
+              style={{ width: "80px", height: "80px" }}
+            />
+            <h2 className="text-black text-xl mb-4 font-bold relative z-20">
+              About
+            </h2>
+            <p className="text-black text-[10px] tracking-wider font-thin relative z-20 mb-16 w-[280px] left-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CricketHero;
--- gsc-frontend/src/pages/Cricket.jsx
+++ gsc-frontend/src/pages/Cricket.jsx
@@ -1,65 +1,29 @@
 import Navbar from "../components/Navbar/Navbar";
-import ImageStack from "../components/Image";
+import achieve from "../images/achieve.svg";
 import ContactCard from "../components/ContactCard";
+import CricketHero from "../components/CricketHero";
 import image1 from "../images/cricket.svg";
+import ImageCarousel from "../components/CarouselSports";
 
 const Cricket = () => {
-  return (
-    <>
-      <Navbar />
-      <div
-        className="h-[530px] lg:h-[704px] md:h-[560px] sm:h-[460px] flex flex-row  justify-center sm:gap-[40px] relative z-[-2]"
-        style={{
-          backgroundImage: `url(${image1})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          width: "100%",
-          height: "894px",
-        }}
-      >
-        <div className="hidden sm:flex flex-col lg:mt-[90px] md:mt-[120px] sm:mt-[100px] items-center gap-[15rem]">
-          <p className=" lg:text-[100px] md:text-[40.5px] sm:text-[25.5px] text-white font-bold font-lexend tracking-tight text-center">
-            Cricket
-          </p>
-          <div className="bg-white bg-opacity-50 border-4 border-white p-4 w-[1019px] h-[392px] font-lexend text-center">
-            <h2 className="text-black text-5xl mb-4 font-bold">About</h2>
-            <p className="text-black text-2xl tracking-wider font-extralight">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
-            </p>
-          </div>
-        </div>
+  const images = [`${image1}`, `${image1}`, `${image1}`];
 
-        <div className="sm:hidden justify-center relative h-[900px]">
-          <p className="w-[100%] text-[60px] text-white font-bold font-lexend tracking-tight text-center mt-24">
-            Cricket
-          </p>
-          <div className="bg-white bg-opacity-50 border-4 border-white p-4 w-[289px] h-[602px] font-lexend text-center top-[16.25rem] absolute">
-            <h2 className="text-black text-2xl font-bold">About</h2>
-            <p className="text-black text-md tracking-wider font-extralight">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
-            </p>
-          </div>
-        </div>
+  return (
+    <div className="bg-[#B9B9B9]">
+      <Navbar />
+      <div style={{ position: "relative", zIndex: "1" }}>
+        <CricketHero />
       </div>
+
       <div>
-        <div className=" bg-white bg-left-bottom bg-[length:305px] ">
-          <p className="w-[100%] sm:text-[100px] text-black font-bold font-lexend text-center tracking-wider text-[40px]">
+        <div className="relative bg-[#B9B9B9] bg-left-bottom bg-[length:305px] mt-44">
+          <p className="absolute top-0 left-0 w-full sm:text-[120px] text-white opacity-50 font-bold font-lexend text-center tracking-wide text-[40px]">
             Facilities
           </p>
-          <div className="relative sm:flex flex-row mx-6 hidden gap-40 pl-10">
-            <div className="w-[741px] h-[476px]">
+          <div className="relative sm:flex flex-row mx-6 hidden gap-40 pl-10 z-10 justify-center">
+            <div className="w-[741px] h-[476px] relative">
               <div>
-                <p className="font-lexend text-xl text-[#000000] tracking-wide">
+                <p className="font-lexend text-xl text-[#000000] tracking-wide absolute top-[120px] left-0">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -71,26 +35,26 @@ const Cricket = () => {
                 </p>
               </div>
             </div>
-            <div className="flex flex-row gap-11">
-              <div className="h-[397px] w-[296px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl">
+            <div className="flex flex-row gap-11 mt-32 mb-16">
+              <div className="h-[360px] w-[296px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl relative">
                 {/* <img
-                src=""
-                className="h-[397px] border-4 border-[#A2A2A2]"
-                alt="GSC_Logo"
-              /> */}
+        src=""
+        className="h-[397px] border-4 border-[#A2A2A2] absolute top-0 left-0"
+        alt="GSC_Logo"
+      /> */}
               </div>
-              <div className="h-[397px] w-[296px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl">
+              <div className="h-[360px] w-[296px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl relative">
                 {/* <img
-                src=""
-                className="h-[397px] border-4 border-[#A2A2A2]"
-                alt="GSC_Logo"
-              /> */}
+        src=""
+        className="h-[397px] border-4 border-[#A2A2A2] absolute top-0 left-0"
+        alt="GSC_Logo"
+      /> */}
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-11 sm:hidden text-center">
-            <div className="flex flex-row gap-3 items-center justify-around">
+          <div className="flex flex-col gap-8 sm:hidden text-center ">
+            <div className="flex flex-row gap-3 items-center justify-around mt-10 z-20">
               <div className="h-[200px] w-[150px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl">
                 {/* <img
                 src=""
@@ -106,7 +70,7 @@ const Cricket = () => {
               /> */}
               </div>
             </div>
-            <p className="font-lexend text-[10px] text-[#000000] my-auto mx-auto px-[35px] justify-center ml-[-68px] xxs:ml-[-30px]">
+            <p className="font-lexend text-[10px] text-[#000000] my-auto mx-auto px-[35px] justify-center items-center flex">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -120,21 +84,53 @@ const Cricket = () => {
         </div>
       </div>
 
-      <div className="h-auto">
-        <div className="bg-white">
-          <p className="w-auto sm:text-[100px] text-[30px] text-black font-bold font-lexend text-center">
+      <div className="mt-10 mb-10">
+        <div className="bg-[#B9B9B9] flex flex-col items-center justify-center gap-10">
+          <p className="w-auto sm:text-[100px] text-[30px] text-white opacity-50 font-bold font-lexend text-center tracking-wide absolute z-10 mb-[330px] sm:mt-0 mt-20">
             Achievements
           </p>
 
-          <div>
-            <ImageStack />
+          <div className="absolute z-10 items-center justify-center mt-40 hidden lg:block">
+            <div className="flex flex-row gap-11 mb-16 items-center justify-center">
+              <div className="h-[360px] w-[296px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl relative">
+                {/* <img
+        src=""
+        className="h-[397px] border-4 border-[#A2A2A2] absolute top-0 left-0"
+        alt="GSC_Logo"
+      /> */}
+              </div>
+              <div className="h-[360px] w-[296px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl relative">
+                {/* <img
+        src=""
+        className="h-[397px] border-4 border-[#A2A2A2] absolute top-0 left-0"
+        alt="GSC_Logo"
+      /> */}
+              </div>
+              <div className="h-[360px] w-[296px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl relative">
+                {/* <img
+        src=""
+        className="h-[397px] border-4 border-[#A2A2A2] absolute top-0 left-0"
+        alt="GSC_Logo"
+      /> */}
+              </div>
+              <div className="h-[360px] w-[296px] bg-[#F4F4F4] border-8 border-[#A2A2A2] rounded-xl relative">
+                {/* <img
+        src=""
+        className="h-[397px] border-4 border-[#A2A2A2] absolute top-0 left-0"
+        alt="GSC_Logo"
+      /> */}
+              </div>
+            </div>
           </div>
+
+          <ImageCarousel images={images} />
+          <img src={achieve} className="relative z-1" />
         </div>
       </div>
 
-      <div className="h-auto flex flex-col justify-center items-center gap-10">
-        <div className="bg-white">
-          <p className="w-full sm:text-[100px] text-[40px] text-black font-bold font-lexend text-center tracking-wider">
+      <div className="h-full flex flex-col justify-center items-center gap-10 bg-[#6690B8]">
+        <div>
+          <p className="w-full sm:text-[100px] text-[40px] text-black font-bold font-lexend text-center tracking-wider sm:mt-0">
             Our Team
           </p>
         </div>
@@ -149,7 +145,7 @@ const Cricket = () => {
           <ContactCard />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
